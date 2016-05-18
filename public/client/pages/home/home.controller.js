1f77c19e6a0b40a8ba327c552b93b9b91e615fb9
anthonyclasson <classon.a@husky.neu.edu>
2016-05-17 20:38:43 -0400

    schema adjustments

--- public/client/pages/home/home.controller.js
+++ public/client/pages/home/home.controller.js
@@ -10,15 +10,15 @@
         $scope.selected = false;
         $scope.search = {};
         
-        $scope.pageSizeOptions = [{label:'10', value: 10}, {label: '25', value: 25}, {label: '50', value: 50}, {label:'100',value:100}, {label: 'All', value: $scope.slen}];
-	    $scope.pageSize = $scope.pageSizeOptions[0].value;
-	    $scope.page = 1;
+         
+  
+
 
         
         HomeService.findAllColumns()
           .then(function(response){
               $scope.columns = response;
-              $scope.keys = Object.keys($scope.columns);
+              $scope.keys = Object.values($scope.columns[0]);
               //shift off mongo id property
               $scope.keys.shift();
               console.log($scope.keys);
@@ -29,6 +29,11 @@
             console.log(category);
             $scope.selected = true;
         }
+        
+	   $scope.pageSizeOptions = [{label:'10', value: 10}, {label: '25', value: 25}, {label: '50', value: 50}, {label:'100',value:100}, {label: 'All', value: $scope.slen}];
+	   $scope.pageSize = $scope.pageSizeOptions[0].value;
+	   $scope.page = 1;
+  
         
   }
 })();
